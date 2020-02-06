ecf4b6c468159a43cb19606e103d20c8379837ec
Ryoji Kitano <ryojikitanodev@gmail.com>
2020-02-06 16:57:28 -0300

    first

--- ChalengeFitPokemonTCG/ChalengeFitPokemonTCG/Program.cs
+++ ChalengeFitPokemonTCG/ChalengeFitPokemonTCG/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
+using System.Linq;
 
 namespace ChalengeFitPokemonTCG
 {
@@ -7,6 +10,36 @@ namespace ChalengeFitPokemonTCG
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var wc = new WebClient();
+
+            for (int i=1; i <= 900; i++) { 
+            string pagina = wc.DownloadString("https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/"+i);
+
+                Console.WriteLine(pagina);
+
+                var htmlDocument = new HtmlDocument();
+
+                htmlDocument.LoadHtml(pagina);
+
+
+                string name = string.Empty;
+            string dado1 = string.Empty;
+
+            foreach(HtmlNode node in htmlDocument.GetElementbyId("report-screen-name-success-modal").ChildNodes)
+            {
+                if(node.Attributes.Count > 0)
+                {
+                    Console.WriteLine(node.SelectNodes("//div/h6")
+                        .First().InnerHtml);
+                }
+            }
+
+            }
+
+           
+
+            
         }
     }
 }
