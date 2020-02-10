b6c974c6ab352eb06a3c775e52ff78ab7b53be04
Ryojidev <ryojikitanodev@gmail.com>
2020-02-09 22:27:35 -0300

    test request cards

--- ChalengeFitPokemonTCG/ChalengeFitPokemonTCG/Program.cs
+++ ChalengeFitPokemonTCG/ChalengeFitPokemonTCG/Program.cs
@@ -22,14 +22,15 @@ namespace ChalengeFitPokemonTCG
 
             var wc = new WebClient();
 
-            for (int i=1; i <= countPage; i++) { 
-            string pagina = wc.DownloadString("https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/"+i);
+            for (int i = 1; i <= countPage; i++)
+            {
+                string pagina = wc.DownloadString("https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ss-series/swsh1/" + i + "/");
 
                 /*Console.WriteLine(pagina);*/
 
                 var htmlDocument = new HtmlDocument();
 
-                
+
 
                 htmlDocument.LoadHtml(pagina);
 
@@ -38,51 +39,70 @@ namespace ChalengeFitPokemonTCG
                 string status = string.Empty;
                 string statusSub = string.Empty;
 
-            foreach(HtmlNode node in htmlDocument.GetElementbyId("report-screen-name-success-modal").ChildNodes)
-            {
-                    
-                    Cards card;
-
-                if (node.Attributes.Count > 0)
+                try
                 {
-                   name = node.SelectNodes("//div/h6")
-                        .First().InnerHtml;
-                    status = node.SelectNodes("//div/h6")
-                        .First().InnerHtml;
-                    statusSub = node.SelectNodes("//div/h6")
-                        .First().InnerHtml;
 
-                         
+                    
 
-                        card = new Cards(i);
+                    foreach (HtmlNode node in htmlDocument.DocumentNode.Descendants())
+                    {
 
-                        
+                        if(htmlDocument.DocumentNode.HasChildNodes && htmlDocument.DocumentNode != null) { 
 
-                        pk.Add(new Pokemon(name, status, statusSub));
+                        Cards card;
 
-                        foreach(Pokemon pkl in pk)
-                        {
-                            Console.WriteLine(pkl.Name);
-                            Console.WriteLine(pkl.Status);
-                            Console.WriteLine(pkl.StatusSub);
+                            if (node.Attributes.Count > 0)
+                            {
+                                name = node.Descendants().First(x => x.Attributes["class"] != null &&
+                                x.Attributes["class"].Value.Equals("color-block color-block-gray")).InnerText;
 
+                                status = WebUtility.HtmlDecode(node.SelectNodes("/html[1]/body[1]/div[4]/section[1]/div[2]/div[1]/ div[2]/h3/div[2]").First().InnerHtml);
+
+
+                                statusSub = WebUtility.HtmlDecode(node.Descendants().First().SelectNodes("/html[1]/body[1]/div[4]/ section[1]/div[2]/div[1]/div[3]/div[4]/h3/a").First().InnerHtml);
+
+
+                                if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(status) && !string.IsNullOrEmpty(statusSub)) { 
+
+                                    card = new Cards(i);
+
+
+
+                                pk.Add(new Pokemon(name, status, statusSub));
+
+                                foreach (Pokemon pkl in pk)
+                                {
+                                    Console.WriteLine(pkl.Name);
+                                    Console.WriteLine(pkl.Status);
+                                    Console.WriteLine(pkl.StatusSub);
+
+
+                                }
+
+
+
+
+
+                            }
 
                         }
 
 
-
-
-
-
+                    }
 
                     }
-            }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("message error" + e.Message);
+                }
+
+
+
 
             }
-
-           
-
-            
         }
     }
+
 }
